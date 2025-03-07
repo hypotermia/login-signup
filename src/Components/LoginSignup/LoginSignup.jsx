8e95da331c98a5186ab75367d3a94f93e60f677e
hypotermia <zakkiaja98@gmail.com>
2025-03-07 20:30:24 +0700

    add API

--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -7,17 +7,49 @@ import password_icon from '../Assets/password.png'
 
 const LoginSignup = () =>{
     const [action,setAction] = useState("Login");
-    const [name,setName] = useState("");
-    const [email,setEmail] = useState("");
-    const [password,setPassword] = useState("");
+    const [firstname,setFirstname] = useState("");
+    const [lastname,setLastname] = useState("");
+    const [username,setEmail] = useState("");
+    const [passwords,setPassword] = useState("");
+    const [error,setError] = useState("");
 
-    const handleSubmit=()=>{
-        console.log("Name:",name);
-        console.log("Email:",email);
-        console.log("Password:",password);
-        setName("");
-        setEmail("");
-        setPassword("");
+    const handleSubmit=async(actionType)=>{
+        
+        setError("");
+        const apiUrl =actionType === "Login"?"https://localhost:44383/api/User/Login":
+        "https://localhost:44383/api/User/AddUser";
+        const body = actionType === "Login" ? {username,passwords}: {username,passwords,firstname,lastname};
+
+        try {
+            const response =await fetch(apiUrl,{
+                method : "POST",
+                headers : {
+                    "Content-Type" : "application/json"
+                },
+                
+                body : JSON.stringify(body)
+                
+            });
+            console.log(body);
+            const data = await response.json();
+            if(!response.ok){
+                throw new Error(data.message || "Gagal melakukan " + actionType);
+            }
+            console.log("Response dari server : ",data);
+            if(actionType==="Login" && data.token){
+                localStorage.setItem("jwtToken",data.token);
+                console.log("Login telah success! Token disimpan");
+            }
+            setLastname("");
+            setFirstname("");
+            setEmail("");
+            setPassword("");
+        }
+        catch (error){
+            setError(error.message);
+            console.error("Error : ", error);
+        }
+
     }
 
     return (
@@ -26,22 +58,28 @@ const LoginSignup = () =>{
                 <div className="text">{action}</div>
                 <div className="underline"></div>
             </div>
+            {error && <div className="error-message">{error}</div>} 
             <div className="inputs">
                 {action==="Login"? <div></div> : <div className="input">
                     <img src={user_icon} alt="" />
-                    <input type="text" placeholder="name"value={name}
-                    onChange={(e)=>setName(e.target.value)}/>
+                    <input type="text" placeholder="Firstname"value={firstname}
+                    onChange={(e)=>setFirstname(e.target.value)}/>
                 </div>}
-
+                {action==="Login"? <div></div> : <div className="input">
+                    <img src={user_icon} alt="" />
+                    <input type="text" placeholder="Lastname"value={lastname}
+                    onChange={(e)=>setLastname(e.target.value)}/>
+                </div>}
+                
                 <div className="input">
                     <img src={email_icon} alt="" />
-                    <input type="email" placeholder="email" value={email}
+                    <input type="email" placeholder="Username" value={username}
                     onChange={(e)=>setEmail(e.target.value)}/>
                 </div>
                 <div className="input">
                     <img src={password_icon} alt="" />
-                    <input type="password" placeholder="password"
-                    value={password}
+                    <input type="password" placeholder="Password"
+                    value={passwords}
                     onChange={(e)=>setPassword(e.target.value)}/>
                 </div>
             </div>
